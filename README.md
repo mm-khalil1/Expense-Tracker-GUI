dd294a1e5a08f949973083f7f3d274900f1e7178
mm-khalil1 <mahmoudmkhalil@hotmail.com>
2024-03-02 20:49:28 +0400

    added data frame, and handeled currency converting

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-### Expense Tracker
+# 💰 Expense Tracker GUI Application 💼
+
+## 🚀 Introduction
+
+This Python script provides a user-friendly interface for tracking expenses, with features like real-time currency conversion, intuitive expense entry, and a sleek display of entered data. It was developed as a final project for the Almdrasa Python course.
+
+## 🔑 Key Features
+
+- **Expense Entry:** Easily input your expenses, including amount, currency, category, payment method, and date.
+- **Currency Conversion:** Seamlessly convert expenses to a base currency (USD) using real-time exchange rates.
+- **Data Display:** View all entered expenses in a tabular format, making it simple to track and manage your spending.
+- **Total Amount Calculation:** Automatically calculates and displays the total amount of expenses entered, providing an instant overview of your spending.
+
+## 🛠️ Dependencies
+
+- **Tkinter:** Python's standard GUI library for creating graphical user interfaces.
+- **requests:** A Python HTTP library used to make API requests for fetching currency data and performing conversions.
+- **datetime:** A Python module for handling date inputs and manipulations.
+
+## 🌐 API Integration
+
+The application integrates with the Apilayer API for fetching currency data and performing conversions. Your API key is securely stored in a separate file ('APILayer_API_Key.env') for authentication.
+
+## 📝 Usage Instructions
+
+1. Ensure that the 'APILayer_API_Key.env' file containing the API key is present in the same directory as the script.
+
+2. Run the script `expense-tracker.py` using the following command in your terminal or command prompt:
+
+```bash
+python expense-tracker.py
+```
+
+3. Enter details for each expense, including amount, currency, category, payment method, and date, then click on the "Add Expense" button to record the expense.
+
+4. View all entered expenses and the total amount in the displayed table.
+
+5. The application handles errors gracefully, displaying appropriate error messages in case of invalid inputs or failed API requests.
+
+## 🖼️ Screenshot
+
+![Expense Tracker](Expense_Tracker_GUI.png)
+
+## ✍️ Author
+
+Created by Mahmoud Khalil
--- expense-tracker.py
+++ expense-tracker.py
@@ -1,41 +1,166 @@
 from tkinter import *
 from tkinter import ttk, messagebox
 from datetime import datetime
+import requests
 
-currency_menu = ["USD", "AED", "SAR"]
-category_menu = ["Life Expenses", "Electricity", "Gas", "Rental", "Grocery", "Savings", "Education", "Charity"]
-payment_menu = ["Cash", "Credit Card", "Paypal"]
-menus = [currency_menu, category_menu, payment_menu]
+# Load API key from file
+with open('APILayer_API_Key.env', 'r') as file:
+    api_key = file.read()
+headers = {"apikey": api_key}
+printrange(5)
+def fetch_currencies_list():
+    """
+    Fetches the list of currencies from the API.
 
-label_names= ["Amount", "Currency", "Category", "Payment Method", "Date"]
+    Returns:
+        list: A list of currency codes.
+    """
+    currencies_list_url = "https://api.apilayer.com/currency_data/list"
 
-def create_label(frame, label_name, relief=FLAT):
-    return Label(frame, text=label_name, relief=relief)
+    try:
+        response = requests.request("GET", currencies_list_url, headers=headers)
+        status_code = response.status_code
+
+        if status_code == 200:
+            data = response.json()
+            currencies = data.get("currencies", {})
+            return list(currencies.keys())
+        else:
+            messagebox.showerror("Error", f"{status_code}. Unable to fetch currencies list.")
+            return []
+    except requests.exceptions.RequestException as e:
+        messagebox.showerror("Error", f"{e}. Unable to make the API request to fetch currencies list.")
+        return None
+
+def currency_converter(amount, base):
+    """
+    Converts an amount from one currency to another.
+
+    Args:
+        amount (float): The amount to be converted.
+        base (str): The currency code of the base amount.
+
+    Returns:
+        float: The converted amount.
+    """
+    target = "USD"
+    if base.upper() == target:
+        return amount
+
+    converter_url = f"https://api.apilayer.com/fixer/convert?to={target}&from={base}&amount={amount}"
+    
+    try:
+        response = requests.request("GET", converter_url, headers=headers)    # Make the API request
+        status_code = response.status_code          # Get the HTTP status code from the response
+
+        if status_code == 200:
+            return response.json()['result']
+        else:
+            messagebox.showerror("Error", f"{status_code}. Unable to fetch total amount conversion rates.")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        messagebox.showerror("Error", f"{e}. Unable to make the API request to convert total amount.")
+        return None
+
+def create_label(frame, label_text, relief=FLAT):
+    """
+    Creates a Label widget.
+
+    Args:
+        frame (Tkinter.Frame): The frame to which the label will be added.
+        label_text (str): The text to be displayed on the label.
+        relief (str): The relief style of the label (default is FLAT).
+
+    Returns:
+        Tkinter.Label: The created Label widget.
+    """
+    return Label(frame, text=label_text, relief=relief)
 
 def create_menu_variable(entry_menu):
+    """
+    Creates a Tkinter StringVar for a Combobox.
+
+    Args:
+        entry_menu (list): A list of menu items.
+
+    Returns:
+        Tkinter.StringVar: The created StringVar.
+    """
     return StringVar(window, value=entry_menu[0])
 
 def create_combobox(menu_var, menu_list):
+    """
+    Creates a Combobox widget.
+
+    Args:
+        menu_var (Tkinter.StringVar): The StringVar to be associated with the Combobox.
+        menu_list (list): A list of menu items.
+
+    Returns:
+        ttk.Combobox: The created Combobox widget.
+    """
     return ttk.Combobox(entries_frame, textvariable=menu_var, values=menu_list, state="readonly")
 
+def display_widgets(widget, row, col, padx=0, pady=0, width=20, sticky="ew", bg=None):
+    """
+    Displays a widget in a specific grid position.
+
+    Args:
+        widget: The widget to be displayed.
+        row (int): The row index.
+        col (int): The column index.
+        padx (int): Padding in the x-direction (default is 0).
+        pady (int): Padding in the y-direction (default is 0).
+        width (int): Width of the widget (default is 20).
+        sticky (str): Alignment of the widget within its grid cell (default is "ew").
+        bg (str): Background color of the widget (default is None).
+    """
+    widget.grid(row=row, column=col, padx=padx, pady=pady, sticky=sticky)
+    widget.config(width=width)
+    if bg is not None:
+        widget.config(bg=bg)
+
 def get_amount():
-    # Get the amount entered and check if it's a valid float
+    """
+    Retrieves the amount entered by the user.
+
+    Returns:
+        float or None: The entered amount if valid, else None.
+    """
     try:
-        return float(amount_entry.get())
+        amount = float(amount_entry.get())
+        if amount < 0:
+            messagebox.showerror("Error", "Amount cannot be negative. Please enter a non-negative number.")
+            return None
+        return amount
     except ValueError:
         messagebox.showerror("Error", "Invalid amount entered. Please enter a valid number.")
         return None
 
 def get_date():
+    """
+    Retrieves the date entered by the user.
+
+    Returns:
+        str or None: The entered date if valid, else None.
+    """
     date_str = date_entry.get()
     try:
-        datetime.strptime(date_str, "%Y-%m-%d")
-        return date_str
+        parsed_date = datetime.strptime(date_str, "%Y-%m-%d")
+        if parsed_date <= datetime.now():           # Check if parsed date is not after today
+            return date_str
+        else:
+            messagebox.showerror("Error", "Date cannot be in the future.")
+            return None
     except ValueError:
         messagebox.showerror("Error", "Invalid date format. Please enter a date in YYYY-MM-DD format.")
         return None
-
+    
 def add_expense():
+    """
+    Adds an expense to the data frame.
+    """
     amount = get_amount()
     if (amount is None):
         return
@@ -45,18 +170,68 @@ def add_expense():
     date = get_date()
     if (date is None):
         return
-        
-    # You can perform further processing with the obtained data, like saving it to a database or file.
-    print("Amount:", amount)
-    print("Currency:", currency)
-    print("Category:", category)
-    print("Payment Method:", payment_method)
-    print("Date:", date)
+    
+    append_expense(amount, currency, category, payment_method, date)
 
-def display_widgets(widget, row, col, padx=0, pady=0, width=None):
-    widget.grid(row=row, column=col, padx=padx, pady=pady, sticky="ew")
-    if width is not None:
-        widget.config(width=width)
+def append_expense(amount, currency, category, payment_method, date):
+    """
+    Appends expense data to the data frame.
+
+    Args:
+        amount (float): The amount of the expense.
+        currency (str): The currency code of the expense.
+        category (str): The category of the expense.
+        payment_method (str): The payment method used for the expense.
+        date (str): The date of the expense.
+    """
+    global total_amount
+    
+    amount_data.append(amount)
+    currency_data.append(currency)
+    category_data.append(category)
+    payment_method_data.append(payment_method)
+    date_data.append(date)
+
+    data_values = [create_label(data_frame, name) 
+                   for name in [amount, currency, category, payment_method, date]]
+    
+    display_data(data_values)
+
+    total_amount += currency_converter(amount, currency)
+    total_amount_value_label.config(text="{:.2f}".format(total_amount))
+
+def display_data(data_values):
+    """
+    Displays expense data in the data frame.
+
+    Args:
+        data_values (list): A list of expense data.
+    """
+    change_previous_data_bg(data_frame, "white")        
+
+    for i, data in enumerate(data_values):
+        display_widgets(data, len(amount_data)-1, i, pady=5, sticky="w", bg="yellow")
+    
+def change_previous_data_bg(frame, new_bg_color):
+    """
+    Changes the background color of previously displayed data.
+
+    Args:
+        frame (Tkinter.Frame): The frame containing the data.
+        new_bg_color (str): The new background color.
+    """
+    for child in frame.winfo_children():
+        if isinstance(child, Label):
+            child.config(bg=new_bg_color)
+
+######################################################
+# Code for initializing and displaying GUI widgets...
+currency_menu = fetch_currencies_list()
+category_menu = ["Life Expenses", "Electricity", "Gas", "Rental", "Grocery", "Savings", "Education", "Charity"]
+payment_menu = ["Cash", "Credit Card", "Paypal"]
+menus = [currency_menu, category_menu, payment_menu]
+
+label_names= ["Amount", "Currency", "Category", "Payment Method", "Date"]
 
 window = Tk()
 window.title("Expense Tracker")
@@ -64,31 +239,28 @@ window.title("Expense Tracker")
 ################ Entries Frame ################
 entries_frame = Frame(window)
 
-## create labels and entries
+# create labels and entries
 entry_labels = [create_label(entries_frame, name) for name in label_names]
 
-amount_entry = Entry(entries_frame, width=20)
+amount_entry = Entry(entries_frame)
 menus_variables = [create_menu_variable(menu_list) for menu_list in menus]
 menus_combobox = [create_combobox(menu_var, menu_list) 
                       for menu_var, menu_list in zip(menus_variables, menus)]
-date_entry = Entry(entries_frame, width=20)
+date_entry = Entry(entries_frame)
 date_entry.insert(0, datetime.now().strftime("%Y-%m-%d"))               # Default to today's date
 entry_widgets = [amount_entry] + menus_combobox + [date_entry]
 
-add_expense_button = Button(entries_frame, text="Add Expense", command=add_expense, width=20)
+add_expense_button = Button(entries_frame, text="Add Expense", command=add_expense)
 
-padx_label = (50, 100) 
-padx_entry = (150, 5) 
-pady_widgets = 5
+padx_label = (5, 100) 
+padx_entry = (100, 5) 
 
 # disaply entries frame layout
 entries_frame.grid(row=0, column=0)
 for i, (entry_label, widget_entry) in enumerate(zip(entry_labels, entry_widgets), start=0):
-    display_widgets(entry_label, i, 0, padx=padx_label, pady=pady_widgets)
-    display_widgets(widget_entry, i, 1, padx=padx_entry, pady=pady_widgets, width=20)
-add_expense_button.grid(row=len(entry_labels), column=1, padx=padx_entry, pady=5, sticky="ew")
-add_expense_button.config(width=20)
-
+    display_widgets(entry_label, i, 0, padx=padx_label, pady=5)
+    display_widgets(widget_entry, i, 1, padx=padx_entry, pady=5)
+display_widgets(add_expense_button, len(entry_labels), 1, padx=padx_entry, pady=5)
 
 ################ header frame ################
 headers_frame = Frame(window)
@@ -96,6 +268,34 @@ header_labels = [create_label(headers_frame, name, relief=GROOVE) for name in la
 
 headers_frame.grid(row=1, column=0)
 for i, header_label in enumerate(header_labels):
-    display_widgets(header_label, 0, i, width=20)
+    display_widgets(header_label, 0, i)
+
+################ Data frame ################
+data_frame = Frame(window, background="white", width=730, height=200)
+
+amount_data = []
+currency_data = []
+category_data = []
+payment_method_data = []
+date_data = []
+
+data_frame.grid(row=2, column=0)
+
+################ Total Amount Frame ################
+total_amount_frame = Frame(window)
+
+total_amount = 0
+for string_var in amount_data:
+    total_amount += string_var.get()
+
+total_amount_label = create_label(total_amount_frame, "Total Amount: ")
+total_amount_value_label = create_label(total_amount_frame, total_amount)
+currency_unit_label = create_label(total_amount_frame, " USD")
+
+total_amount_frame.grid(row=3, column=0)
+display_widgets(total_amount_label, 0, 0, pady=5, width=0, sticky='w', bg=None)
+display_widgets(total_amount_value_label, 0, 1, pady=5, width=0, sticky='w', bg=None)
+display_widgets(currency_unit_label, 0, 2, pady=5, width=0, sticky='w', bg=None)
+
 
 window.mainloop()
